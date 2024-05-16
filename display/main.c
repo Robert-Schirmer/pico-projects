a95a252188f0347467ed2c3490e311811ceb8226
Robert Schirmer <rhsblue@gmail.com>
2024-05-16 00:01:12 -0400

    display: function sliding_words_display

--- display/main.c
+++ display/main.c
@@ -6,43 +6,85 @@
 #include "ssd1306.h"
 
 #define SLEEPTIME 25
-#define IC2_BUS i2c1
 
-void setup_gpios(void);
-void animation(void);
+i2c_inst_t *setup_gpios(void);
+ssd1306_t setup_dislay(i2c_inst_t *p_i2c_bus);
+void animate_infinitely(ssd1306_t *disp);
+void sliding_words_display(ssd1306_t *disp, char *words, int font_size[], int screen_size[]);
 
 int main()
 {
     stdio_init_all();
 
-    printf("configuring pins...\n");
-    setup_gpios();
+    i2c_inst_t *p_i2c_bus = setup_gpios();
+    ssd1306_t disp = setup_dislay(p_i2c_bus);
 
-    printf("jumping to animation...\n");
-
-    animation();
+    char *greeting = "Helllloooooooooooooo, World! How you doing today, I got a long string here for you. Hope you can read all of it while the screen scrolls :D. It's a long one, so I hope you're ready for it! Still going ................. and ...... done nice work";
+    for (;;)
+    {
+        sliding_words_display(&disp, greeting, (int[]){8, 6}, (int[]){32, 128});
+    }
 }
 
-void setup_gpios(void)
+void sliding_words_display(ssd1306_t *disp, char *string, int font_size[], int screen_size[])
 {
-    const uint sda_pin = 18;
-    const uint scl_pin = 19;
+    int str_len = strlen(string);
+    // Sizes are in pixels height x width, font width should include spacing between characters
+    double characters_on_screen = screen_size[1] / font_size[1];
+    int characters_off_screen = str_len - characters_on_screen;
+    int slide_times = characters_off_screen + 1;
 
-    i2c_init(IC2_BUS, 400 * 1000);
+    printf("characters_on_screen: %f\n", characters_on_screen);
+    printf("characters_off_screen: %d\n", characters_off_screen);
+    printf("slide_times: %d\n", slide_times);
+
+    for (int i = 0; i < slide_times; ++i)
+    {
+        ssd1306_clear(disp);
+
+        char *word_chopped = string + i;
+        ssd1306_draw_string(disp, 1, 1, 1, word_chopped);
+
+        ssd1306_show(disp);
+        if ((i == 0 && slide_times > 1) || i == slide_times - 1)
+            // Longer sleep for start and end of sliding
+            sleep_ms(2000);
+        else
+            sleep_ms(100);
+    }
+}
+
+i2c_inst_t *setup_gpios(void)
+{
+    uint sda_pin = 18;
+    uint scl_pin = 19;
+    uint baudrate = 400 * 1000;
+    i2c_inst_t *i2c_bus = i2c1;
+
+    i2c_init(i2c_bus, baudrate);
     gpio_set_function(sda_pin, GPIO_FUNC_I2C);
     gpio_set_function(scl_pin, GPIO_FUNC_I2C);
     gpio_pull_up(sda_pin);
     gpio_pull_up(scl_pin);
+
+    return i2c_bus;
 }
 
-void animation(void)
+ssd1306_t setup_dislay(i2c_inst_t *p_i2c_bus)
+{
+    ssd1306_t disp;
+    disp.external_vcc = false;
+    ssd1306_init(&disp, 128, 32, 0x3C, p_i2c_bus);
+    ssd1306_clear(&disp);
+
+    return disp;
+}
+
+void animate_infinitely(ssd1306_t *p_disp)
 {
     const char *words[] = {"SSD1306", "DISPLAY", "DRIVER"};
 
-    ssd1306_t disp;
-    disp.external_vcc = false;
-    ssd1306_init(&disp, 128, 32, 0x3C, IC2_BUS);
-    ssd1306_clear(&disp);
+    ssd1306_clear(p_disp);
 
     printf("ANIMATION!\n");
 
@@ -50,39 +92,39 @@ void animation(void)
     {
         for (int y = 0; y < 31; ++y)
         {
-            ssd1306_draw_line(&disp, 0, y, 127, y);
-            ssd1306_show(&disp);
+            ssd1306_draw_line(p_disp, 0, y, 127, y);
+            ssd1306_show(p_disp);
             sleep_ms(SLEEPTIME);
-            ssd1306_clear(&disp);
+            ssd1306_clear(p_disp);
         }
 
         for (int y = 0, i = 1; y >= 0; y += i)
         {
-            ssd1306_draw_line(&disp, 0, 31 - y, 127, 31 + y);
-            ssd1306_draw_line(&disp, 0, 31 + y, 127, 31 - y);
-            ssd1306_show(&disp);
+            ssd1306_draw_line(p_disp, 0, 31 - y, 127, 31 + y);
+            ssd1306_draw_line(p_disp, 0, 31 + y, 127, 31 - y);
+            ssd1306_show(p_disp);
             sleep_ms(SLEEPTIME);
-            ssd1306_clear(&disp);
+            ssd1306_clear(p_disp);
             if (y == 32)
                 i = -1;
         }
 
         for (int i = 0; i < sizeof(words) / sizeof(char *); ++i)
         {
-            ssd1306_draw_string(&disp, 1, 1, 1, words[i]);
-            ssd1306_show(&disp);
+            ssd1306_draw_string(p_disp, 1, 1, 1, words[i]);
+            ssd1306_show(p_disp);
             sleep_ms(800);
-            ssd1306_clear(&disp);
+            ssd1306_clear(p_disp);
         }
 
         for (int y = 31; y >= 0; --y)
         {
-            ssd1306_draw_line(&disp, 0, y, 127, y);
-            ssd1306_show(&disp);
+            ssd1306_draw_line(p_disp, 0, y, 127, y);
+            ssd1306_show(p_disp);
             sleep_ms(SLEEPTIME);
-            ssd1306_clear(&disp);
+            ssd1306_clear(p_disp);
         }
 
-        ssd1306_show(&disp);
+        ssd1306_show(p_disp);
     }
 }
