11e894ef0339538de765bd098d2defabcc4f2f47
Robert Schirmer <rhsblue@gmail.com>
2024-07-23 20:48:16 -0400

    plantingtos: backwards read log

--- plantingtosh/process_log.py
+++ plantingtosh/process_log.py
@@ -1,9 +1,55 @@
+import os
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 import datetime
 import numpy as np
 from scipy.interpolate import interp1d
 from scipy import signal
+import time
+
+class BackwardsReader:
+  def readline(self):
+    while len(self.data) == 1 and ((self.blkcount * self.blksize) < self.size):
+      self.blkcount = self.blkcount + 1
+      line = self.data[0]
+      try:
+        self.f.seek(-self.blksize * self.blkcount, 2) # read from end of file
+        self.data = str.split(str(self.f.read(self.blksize),'utf-8') + line, '\n')
+      except IOError:  # can't seek before the beginning of the file
+        self.f.seek(0)
+        self.data = str.split(str(self.f.read(self.size - (self.blksize * (self.blkcount-1))), 'utf-8') + line, '\n')
+
+    if len(self.data) == 0:
+      return ""
+
+    # self.data.pop()
+    # make it compatible with python <= 1.5.1
+    line = self.data[-1]
+    self.data = self.data[:-1]
+    return line + '\n'
+  
+  def close(self):
+    self.f.close()
+
+  def __init__(self, file, blksize=4096):
+    """initialize the internal structures"""
+    # get the file size
+    self.size = os.stat(file)[6]
+    # how big of a block to read from the file...
+    self.blksize = blksize
+    # how many blocks we've read
+    self.blkcount = 1
+    self.f = open(file, 'rb')
+    # if the file is smaller than the blocksize, read a block,
+    # otherwise, read the whole thing...
+    if self.size > self.blksize:
+      self.f.seek(-self.blksize * self.blkcount, 2) # read from end of file
+    self.data = str(self.f.read(self.blksize), 'utf-8').split('\n')
+    # strip the last item if it's empty...  a byproduct of the last line having
+    # a newline at the end of it
+    if not self.data[-1]:
+      # self.data.pop()
+      self.data = self.data[:-1]
 
 def celcius_to_fahrenheit(celcius):
   return celcius * 9 / 5 + 32
@@ -13,47 +59,70 @@ def linspace_datetime64(start_date, end_date, n):
 
 cleaned_data = []
 bad_data = []
+timestamps = []
 
-with open("log.txt") as f:
-  for line in f:
-    fields = {}
-    fields_list = line.strip().split(',')
+start = time.time()
+
+log_file = "log.txt"
+
+backwards_reader = BackwardsReader(log_file)
+
+read_back_days = 100
+end_timestamp = int((datetime.datetime.now() - datetime.timedelta(read_back_days)).strftime('%s'))
+
+done = False
+while not done:
+  line = backwards_reader.readline()
+  if not line:
+    done = True
+    continue
+
+  fields = {}
+  fields_list = line.strip().split(',')
+  
+  for field in fields_list:
+    key, value = field.split('=')
+    fields[key] = value
+  
+  if len(fields.get('received')) != len('1718406938144'):
+    bad_data.append(fields)
+    continue
+  
+  temp = int(fields.get('temp'))
+  
+  if temp < 2 or temp > 400:
+    bad_data.append(fields)
+    continue
+  
+  capacitence = int(fields.get('capacitence'))
+  
+  if capacitence < 100 or capacitence > 600:
+    bad_data.append(fields)
+    continue
     
-    for field in fields_list:
-      key, value = field.split('=')
-      fields[key] = value
-    
-    if len(fields.get('received')) != len('1718406938144'):
-      bad_data.append(fields)
-      continue
-    
-    temp = int(fields.get('temp'))
-    
-    if temp < 2 or temp >400:
-      bad_data.append(fields)
-      continue
-    
-    capacitence = int(fields.get('capacitence'))
-    
-    if capacitence < 100 or capacitence > 600:
-      bad_data.append(fields)
-      continue
-      
-    # Only one plant for now with this ID, so all data is for this plant
-    if fields.get('plant_id') != 'E6616408435E092D':
-      fields['plant_id'] = 'E6616408435E092D'
-    
-    cleaned_data.append(fields)
-    
+  # Only one plant for now with this ID, so all data is for this plant
+  if fields.get('plant_id') != 'E6616408435E092D':
+    fields['plant_id'] = 'E6616408435E092D'
+
+  timestamp = int(fields.get('received')) / 1000
+
+  if timestamp < end_timestamp:
+    done = True
+    print(f"Found end timestamp: {timestamp}")
+    continue
+
+  timestamps.append(timestamp)
+  cleaned_data.append(fields)
+
+backwards_reader.close()
+
+cleaned_data.reverse()
+timestamps.reverse()
+
+end = time.time()
+
+print(f"Read {len(cleaned_data)} records in {end - start} s from {log_file}")
 print(f"Found {len(bad_data)} bad records.")
-print(bad_data)
-
-# with open("cleaned_data.txt", "w") as f:
-#   for fields in cleaned_data:
-#     line = ",".join([f"{key}={value}" for key, value in fields.items()])
-#     f.write(line + "\n")
-
-timestamps = [int(fields.get('received')) / 1000 for fields in cleaned_data]
 
 capacitence = [int(fields.get('capacitence')) for fields in cleaned_data]
 temp = [celcius_to_fahrenheit(int(fields.get('temp')) / 10) for fields in cleaned_data]
@@ -79,6 +148,23 @@ f_capacitence = interp1d(timestamps, capacitence_filtered, kind='linear')
 resampled_timestamps = linspace_datetime64(timestamps[0], timestamps[-1], int((timestamps[-1] - timestamps[0]) / (60 * 60)))
 temp_smooth = f_temp(resampled_timestamps)
 capacitence_smooth = f_capacitence(resampled_timestamps)
+d_capacitence_smooth = np.gradient(capacitence_smooth)
+
+# Amount of time to wait before marking another water event
+water_time_cooldown = 60 * 60 * 3 # seconds
+last_water_time = 0 # seconds
+# Amount of change in capacitence to trigger a water event
+d_capacitence_for_watering = 10
+
+water_markers = []
+
+for i in range(0, len(capacitence_smooth)):
+  if d_capacitence_smooth[i] > d_capacitence_for_watering and (resampled_timestamps[i] - last_water_time) > water_time_cooldown:
+    water_markers.append(capacitence_smooth[i])
+    print(f"Watered at {datetime.datetime.fromtimestamp(resampled_timestamps[i])}, dCapacitence: {d_capacitence_smooth[i]}")
+    last_water_time = resampled_timestamps[i]
+  else:
+    water_markers.append(None)
 
 # Plot
 plot_raw = True # Plot the raw data points
@@ -87,6 +173,8 @@ fig, ax1 = plt.subplots()
 ax=plt.gca()
 xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
 ax.xaxis.set_major_formatter(xfmt)
+plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+ax.xaxis.set_visible(False)
 
 resampled_datetime = [datetime.datetime.fromtimestamp(x) for x in resampled_timestamps]
 timestamps_datetime = [datetime.datetime.fromtimestamp(x) for x in timestamps]
@@ -107,6 +195,8 @@ ax2.plot(resampled_datetime, capacitence_smooth, color=color)
 if (plot_raw):
   ax2.scatter(timestamps_datetime, capacitence, color=color, alpha=0.1, s=1)
 ax2.tick_params(axis='y', labelcolor=color)
+
+ax2.plot(resampled_datetime, water_markers, 'go')
 
 fig.tight_layout()
 plt.show()
